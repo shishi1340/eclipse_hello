a7a61d2ce4d61f5d1034078dd2e5f3704180388c
shlee <shishi1340@naver.com>
2019-04-03 10:55:51 +0900

    merge and commit

--- test_git/PushPrint.py
+++ test_git/PushPrint.py
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
 print("Add Python file")
 print("Hello i'm local")
-=======
 print("Add Python file")
-print("Hello, I'm github")
->>>>>>> refs/remotes/origin/master
+print("Hello hello hello")
